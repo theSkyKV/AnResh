bf243501b35a9a493a49cea1e0c2b3c0696ed078
theSkyKV <thesky.kv@gmail.com>
2021-09-15 14:55:49 +0300

    stage 1 done

---
+++ AnResh/Controllers/UserInputController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AnResh.CsharpScripts;
+using AnResh.Models;
+
+namespace AnResh.Controllers
+{
+    public class UserInputController : Controller
+    {
+        public ActionResult Index()
+        {
+            var userInput = new UserInput();
+            return View(userInput);
+        }
+
+        [HttpPost]
+        [MultiButton(MatchFormKey = "action", MatchFormValue = "UpdateFromFile")]
+        public ActionResult UpdateFromFile(UserInput userInput)
+        {
+            userInput.UpdateFromFile();
+            return View(userInput);
+        }
+
+        [HttpPost]
+        [MultiButton(MatchFormKey = "action", MatchFormValue = "SaveToFile")]
+        public ActionResult SaveToFile(UserInput userInput, string text)
+        {
+            userInput.SaveToFile(text);
+            return View(userInput);
+        }
+    }
+}
---
+++ AnResh/Models/UserInput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace AnResh.Models
+{
+    public class UserInput
+    {
+        private string _path = AppDomain.CurrentDomain.BaseDirectory + "App_Data\\Text.txt";
+
+        public string Text { get; private set; }
+
+        public void UpdateFromFile()
+        {
+            Text = File.ReadAllText(_path);
+        }
+
+        public void SaveToFile(string text)
+        {
+            File.WriteAllText(_path, text);
+        }
+    }
+}
