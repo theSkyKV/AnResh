963a067ad8577ea8e8afcb287774d9c4f49d67b1
theSkyKV <thesky.kv@gmail.com>
2021-09-22 13:11:08 +0300

    added models and connection to db

---
+++ AnResh/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnResh.Models
+{
+    public class Department
+    {
+        public string DepartmentName { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
