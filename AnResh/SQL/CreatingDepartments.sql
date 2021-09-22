c0ca2ae7a8eaac39abc932deaa7a3d5a5716e3d2
theSkyKV <thesky.kv@gmail.com>
2021-09-22 11:59:18 +0300

    add init db scripts

---
+++ AnResh/SQL/CreatingDepartments.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Departments (
+DepartmentName NVARCHAR(50) NOT NULL,
+DepartmentId INT IDENTITY NOT NULL PRIMARY KEY
+);
+
+INSERT INTO Departments (DepartmentName)
+VALUES
+('Management'),
+('Financial'),
+('Development');
